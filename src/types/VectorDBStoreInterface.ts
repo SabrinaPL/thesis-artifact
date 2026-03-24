8453779279b186f2826df3913bf0067a5637aab9
Bea Sanssi <bs222eh@student.lnu.se>
2026-03-24 12:25:59 +0100

    WIP: update vector store db interface befor trying to ingest document

--- src/types/VectorDBStoreInterface.ts
+++ src/types/VectorDBStoreInterface.ts
@@ -1,4 +1,8 @@
 export interface VectorDBStoreInterface {
   // TODO: define the methods that any vector DB store must implement
-  insertToDB(rawDocument: string, metaData: object): Promise<void>
+  // insertToDB(rawDocument: string, metaData: object): Promise<void>
+  insertToDB(
+    text: string,
+    metadata: Record<string, unknown>
+  ): Promise<void>;
 }
